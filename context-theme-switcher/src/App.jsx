62262bde4ac4f38e2d2eefe2f6fbf019c199270e
Chirag Arya <carya6785@gmail.com>
2026-06-14 14:37:35 +0530

    bring componets only

--- context-theme-switcher/src/App.jsx
+++ context-theme-switcher/src/App.jsx
@@ -1,8 +1,15 @@
-
 function App() {
   return (
-    <div className="text-3xl font-bold underline">App</div>
-  )
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* Theme switcherBtn */}
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">{/* card component */}</div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
