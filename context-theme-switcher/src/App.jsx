38cea339978edfddc3f7bc0b64c8f80bfd8455fc
Chirag Arya <carya6785@gmail.com>
2026-06-14 14:53:47 +0530

    fix: correct ThemeProvider export name and wrap App component with ThemeProvider

--- context-theme-switcher/src/App.jsx
+++ context-theme-switcher/src/App.jsx
@@ -1,14 +1,18 @@
+import { ThemeProvider } from "./contexts/theme";
+
 function App() {
   return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* Theme switcherBtn */}
-        </div>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* Theme switcherBtn */}
+          </div>
 
-        <div className="w-full max-w-sm mx-auto">{/* card component */}</div>
+          <div className="w-full max-w-sm mx-auto">{/* card component */}</div>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
