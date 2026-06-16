cf8d264896b28687a449ecb18727f6992a53210f
Chirag Arya <carya6785@gmail.com>
2026-06-16 19:23:55 +0530

    feat: refactor App component and provide store in main file

--- react-redux-toolKit/src/App.jsx
+++ react-redux-toolKit/src/App.jsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>React Redux Toolkit</h1>
+      <h1 className="text-3xl font-bold underline">React Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
