b467bcf5052cfc795bfe54697424b066862f1dcd
Chirag Arya <carya6785@gmail.com>
2026-06-16 18:33:42 +0530

    feat: create todoSlice with add, remove, and update reducers and proper export

---
+++ react-redux-toolKit/src/features/todo/todoSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, todoText: "first todo" }],
+};
+
+export const todoSlice = createSlice({
+  name: "Todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        todoText: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      const id = action.payload;
+      state.todos = state.todos.filter((currTodo) => currTodo.id !== id);
+    },
+  },
+  updateTodo: function (state, action) {
+    const id = action.payload;
+    const newTodoText = action.payload;
+
+    state.todos = state.todos.map((currTodo) =>
+      currTodo.id === id ? { ...currTodo, todoText: newTodoText } : currTodo,
+    );
+  },
+});
+
+export const {addTodo,removeTodo,updateTodo} = todoSlice.actions
+
+export default createSlice.reducers
